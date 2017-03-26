17a979ae2b5b54cc5537e22180e22d6cfbb7daca
q3689852 <3689852@qq.com>
2017-03-26 14:39:46 +0800

    模式1模式2完成

--- js/js.js
+++ js/js.js
@@ -14,6 +14,8 @@ window.onload=function()
 				oIpt[i].index=i;
 				oIpt[i].onclick=function()
 				{
+					
+					document.getElementById('right_body').style.background="#DDDDDD";
 					for(var i = 0 ;i<oIpt.length;i++)
 					{	
 						oIpt[i].className="mode";
@@ -22,56 +24,45 @@ window.onload=function()
 					this.className="mode";
 					aDiv[this.index].style.display="block";
 				}
-			}
-			
-			
-			
-			oMo_but[0].onclick=function()
-			{
-				var Bipt = document.getElementById("biaoti");
-				var Tipt = document.getElementById("tupian");
-				var Tipt = r_div.getElementsByClassName("mode_text");
-			}
-			
-			
-			
-			
-			oMo_but[0].onclick=function()
-			{
-				var Bipt = document.getElementById("biaoti");
-				var Tipt = document.getElementById("tupian");
-				var Tipt = r_div.getElementsByClassName("mode_text");
-				
-				var a = "&lt;"+"?xml version="+"&quot;"+"1.0"+"&quot;"+" encoding="+"&quot;"+"utf-8"+"&quot;"+"?"+"&gt;"+
-						"&lt;"+"msg flag="+"&quot;"+"0"+"&quot;"+" serviceID="+"&quot;"+"77"+"&quot;"+" brief="+"&quot;"+"新消息"+"&quot;"+" templateID="+"&quot;"+"14"+"&quot;"+" action="+"&quot;"+"web"+"&quot;"+"&gt;"+
-						"&lt;"+"item layout='3'"+"&gt;"+"&lt"+"button action="+"&quot;"+"web"+"&quot;"+" size="+"&quot;"+"30"+"&quot;"+" color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
-				var b = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+"item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt;"+"/hr"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+"item layout="+"&quot;"+"0"+"&quot;"+"&gt;"+"&lt;"+"&quot;"+"picture cover="+"&quot;"
-				var c = "&quot;"+"/"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt"+"/ht"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色3】"+"&quot;"+"&gt;"
-				var d = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
-				var e = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色6】"+"&quot;"+"&gt;"
-				var f = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt"+"/ht"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
-				var g = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色5】"+"&quot;"+"&gt;"
-				var h = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色8】"+"&quot;"+"&gt;"
-				var i = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt"+"/ht"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色5】"+"&quot;"+"&gt;"
-				var j = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
-				var k = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色8】"+"&quot;"+"&gt;"
-				var l = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt"+"/ht"+"&gt;"+"&lt;"+"/item"+"&gt;"+
-						"&lt;"+" item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
-				var m = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色8】"+"&quot;"+"&gt;"
-				var n = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"size="+"&quot"+"30"+"&quot;"+"color="+"&quot;"+"【百变颜色7】"+"&quot;"+"&gt;"
-				var o = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+"&lt;"+"/msg"+"&gt;"+"&nbsp;"
-				document.write(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o);
-			}
-			oMo_but[1].onclick=function()
+			}	
+		oMo_but[0].onclick=function()
+{
+	
+	
+
+	var Tipt = r_div.getElementsByClassName("mode_text");
+	
+	
+	var a = "&lt;"+"?xml version="+"&quot;"+"1.0"+"&quot;"+" encoding="+"&quot;"+"utf-8"+"&quot;"+"?"+"&gt;"+
+			"&lt;"+"msg flag="+"&quot;"+"0"+"&quot;"+" serviceID="+"&quot;"+"77"+"&quot;"+" brief="+"&quot;"+"新消息"+"&quot;"+" templateID="+"&quot;"+"14"+"&quot;"+"  action="+"&quot;"+"web"+"&quot;"+"&gt;"+
+			"&lt;"+"item layout='3'"+"&gt;"+"&lt"+"button action="+"&quot;"+"web"+"&quot;"+"   size="+"&quot;"+"30"+"&quot;"+" color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
+	var b = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt;"+"/hr"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item layout="+"&quot;"+"0"+"&quot;"+"&gt;"+"&lt;"+"picture cover="+"&quot;"
+	var c = "&quot;"+"/"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt;"+"/hr"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+" color="+"&quot;"+"【百变颜色3】"+"&quot;"+"&gt;"
+	var d = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
+	var e = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色6】"+"&quot;"+"&gt;"
+	var f = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt;"+"/hr"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+" color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
+	var g = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色5】"+"&quot;"+"&gt;"
+	var h = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色8】"+"&quot;"+"&gt;"
+	var i = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt;"+"/hr"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色5】"+"&quot;"+"&gt;"
+	var j = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
+	var k = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色8】"+"&quot;"+"&gt;"
+	var l = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item"+"&gt;"+"&lt;"+"hr"+"&gt;"+"&lt;"+"/hr"+"&gt;"+"&lt;"+"/item"+"&gt;"+
+			"&lt;"+"item layout="+"&quot;"+"3"+"&quot;"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色9】"+"&quot;"+"&gt;"
+	var m = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色8】"+"&quot;"+"&gt;"
+	var n = "&lt;"+"/button"+"&gt;"+"&lt;"+"button action="+"&quot"+"web"+"&quot"+"  size="+"&quot"+"30"+"&quot;"+"  color="+"&quot;"+"【百变颜色7】"+"&quot;"+"&gt;"
+	var o = "&lt;"+"/button"+"&gt;"+"&lt;"+"/item"+"&gt;"+"&lt;"+"/msg"+"&gt;"+"&nbsp;"
+	document.write(a+b+c+d+e+f+g+h+i+j+k+l+m+n+o);
+}
+		oMo_but[1].onclick=function()
 			{
 				var Tipt = r_div.getElementsByClassName("mode2_text");	
 				var okdiv = document.getElementsByClassName("mode_img")[1];
@@ -89,7 +80,7 @@ window.onload=function()
 				
 			
 				
-			}	
+			}
+		
 		}
 		
-
